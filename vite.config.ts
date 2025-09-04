3ebfc9f4ef19cbc655b612e160b22a218d0dbff9
NandoVerdu <fverdu@sanantonio1.org>
2025-09-04 23:27:00 +0200

    Configuración posterior  del proyecto

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,4 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 import { viteSingleFile } from 'vite-plugin-singlefile'
 
 // https://vitejs.dev/config/
@@ -7,5 +6,5 @@ export default defineConfig({
   // Para el despliegue en GitHub Pages, 'base' debe ser el nombre de tu repositorio.
   // Lo he actualizado para que coincida con tu nuevo repositorio.
   base: '/stepsProgramacionDidactica/', 
-  plugins: [react(), viteSingleFile()],
+  plugins: [ viteSingleFile()],
 })
